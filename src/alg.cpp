77251fba3a048081a5d3a0cd9350d9b9b0056e65
KseniiaMatveeva <kseniiamatv@gmail.com>
2022-06-08 17:14:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,28 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+  BST<std::string> newTree;
+  std::string slova = "";
+  const char* filename = "war_peace.txt"
+  std::ifstream file(filename);
+  char raznic = 'a' - 'A';
+  if (!file) {
+    std::cout << "File error!" << std::endl;
+      return;
+  } else { 
+    while(!file.eof()) {
+      char sim = file.get();
+      if ((sim >= 'a' && sim <= 'z') || (sim >= 'A' && sim <= 'Z')) {
+        if (sim >= 'A' && sim <= 'Z') {
+          sim = sim + raznic;
+        }
+        slova = slova + sim;
+      } else if (slova != "") {
+        newTree.add(slova);
+        slova = "";
+      }
+    }
+    file.close();
+    return newTree;
+  }
 }
